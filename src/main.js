830154a8e7ea8e8ff5fd5834cdcf05046a238486
buxianshan <583425306@qq.com>
2022-04-04 15:09:32 +0800

    ElementUI改为按需引入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import { Button } from 'element-ui'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Button)
 
 new Vue({
   render: h => h(App),
