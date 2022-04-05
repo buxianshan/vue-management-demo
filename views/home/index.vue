8491071cdc55bb2c3f4f97bec65fe954a2755e4f
buxianshan <583425306@qq.com>
2022-04-05 18:07:41 +0800

    订单统计数据卡片

--- views/home/index.vue
+++ views/home/index.vue
@@ -20,6 +20,18 @@
         </el-table>
       </el-card>
     </el-col>
+    <el-col :span="16" style="margin-top: 20px">
+      <div class="num">
+        <el-card v-for="item in countData" :key="item.name" :body-style="{display: 'flex', padding: 0}">
+          <i class="icon" :class="`el-icon-${item.icon}`" :style="{backgroundColor: item.color}"></i>
+          <div class="detail">
+            <p class="num">￥{{item.value}}</p>
+            <p class="txt">{{item.name}}</p>
+          </div>
+        </el-card>
+      </div>
+
+    </el-col>
   </el-row>
 </template>
 
@@ -29,51 +41,88 @@ export default {
   data() {
     return {
       userIcon: require('../../src/assets/images/user.png'),
-      tableData:
-          [
-            {
-              name: 'oppo',
-              todayBuy: 100,
-              monthBuy: 300,
-              totalBuy: 800
-            },
-            {
-              name: 'vivo',
-              todayBuy: 100,
-              monthBuy: 300,
-              totalBuy: 800
-            },
-            {
-              name: '苹果',
-              todayBuy: 100,
-              monthBuy: 300,
-              totalBuy: 800
-            },
-            {
-              name: '小米',
-              todayBuy: 100,
-              monthBuy: 300,
-              totalBuy: 800
-            },
-            {
-              name: '三星',
-              todayBuy: 100,
-              monthBuy: 300,
-              totalBuy: 800
-            },
-            {
-              name: '魅族',
-              todayBuy: 100,
-              monthBuy: 300,
-              totalBuy: 800
-            }
-          ],
+      tableData: [
+        {
+          name: 'oppo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: 'vivo',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '苹果',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '小米',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '三星',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        },
+        {
+          name: '魅族',
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800
+        }
+      ],
       tableLabel: {
         name: '课程',
         todayBuy: '今日购买',
         monthBuy: '本月购买',
         totalBuy: '总购买',
       },
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "今日收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "今日未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+        {
+          name: "本月支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9",
+        },
+        {
+          name: "本月收藏订单",
+          value: 210,
+          icon: "star-on",
+          color: "#ffb980",
+        },
+        {
+          name: "本月未支付订单",
+          value: 1234,
+          icon: "s-goods",
+          color: "#5ab1ef",
+        },
+      ],
     }
   }
 }
